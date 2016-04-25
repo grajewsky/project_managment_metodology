434cd98febe88ad60f504b155329fcaa8e6d444f
Grajewsky <grajewski.lukasz@wp.pl>
2016-04-26 01:23:02 +0200

    Merge Total
    Progmra zmergowany

--- src/main.java
+++ src/main.java
@@ -8,11 +8,14 @@ public class main {
         onload load=  new onload();
         sumacj _sumacj = new sumacj();
         cmax _cmax = new cmax();
+        lmax _lmax = new lmax();
         _sumacj.setMenu("Problematyka 1||Suma Cj");
         _cmax.setMenu("Problematyka 1||Cmax");
+        _lmax.setMenu("Problematyka 1||Lmax");
 
         load.problematyki.put(1, _sumacj);
         load.problematyki.put(2, _cmax);
+        load.problematyki.put(3, _lmax);
         load.input();
         load.todo();
         load.output();
